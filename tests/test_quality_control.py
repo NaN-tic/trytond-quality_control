0af84df5652fb7ca21c90272c1d39f8f79c3f034
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:45 +0200

    Update test to version 4.0

--- tests/test_quality_control.py
+++ tests/test_quality_control.py
@@ -1,23 +1,25 @@
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+#!/usr/bin/env python
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import doctest
+import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import suite as test_suite
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import doctest_checker
 
 
-class QualityControlTestCase(ModuleTestCase):
-    'Test Quality Control module'
+class TestCase(ModuleTestCase):
+    'Test module'
     module = 'quality_control'
 
 
 def suite():
-    suite = test_suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            QualityControlTestCase))
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
     suite.addTests(doctest.DocFileSuite(
             'scenario_quality_test.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
